bfc00fb01682103240432d53af649304ba9e1ab3
demureaxs <118724010+Demureaxs@users.noreply.github.com>
2023-01-26 23:15:44 +0700

    landing page optimisations

--- pages/index.js
+++ pages/index.js
@@ -19,24 +19,27 @@ export default function Home() {
               Say goodbye to endless resumes and interviews
             </h1>
             <h2 className="text-6xl font-extrabold max-w-[600px] text-center">
-              Experience the future of hiring..
+              Experience the future of hiring...
             </h2>
           </div>
         </section>
         <section className="flex justify-center items-center mt-[-300px] bg-gradient-to-br  from-slate-100 to-slate-200 p-40 text-slate-600">
-          <div className=" h-[800px] flex pt-52 justify-between ">
+          <div className=" h-fit mx-auto sm:h-[800px] flex pt-52 justify-between ">
             <div className="flex flex-col justify-center items-center">
-              <p className=" pb-2 text-3xl w-[800px] font-semibold text-center leading-normal">
-                With StaffLink, you can say goodbye to hours spent sifting
-                through resumes and interviewing countless candidates.
+              <p className=" text-lg pb-2 sm:text-3xl max-w-[800px] font-semibold text-center leading-normal">
+                With Staff<span>Link</span>, you can say goodbye to hours spent
+                sifting through resumes and interviewing countless candidates.
               </p>
-              <p className="text-4xl text-center font-semibold">
+              <p className=" text-xl  sm:text-4xl text-center font-semibold sm:max-w-[900px]">
                 Our innovative technology streamlines the recruitment process,
                 allowing you to find the best candidates for your open positions
                 in just minutes.
               </p>
             </div>
           </div>
+        </section>
+        <section className="clip2 h-[1200px] bg-green-500 mt-[-180px]">
+          <div></div>
         </section>
 
         <FooterTwo />
